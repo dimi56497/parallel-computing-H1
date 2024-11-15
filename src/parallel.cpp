82f8560c9cd290dc66b19ab3d6fc6cf67eb3a5e5
Dimitri Corraini <dimi56497@yahoo.com>
2024-11-15 08:35:21 +0100

    feat: add serial code

---
+++ src/parallel.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#define N_SIZE 1000
+
+int main() {
+    return 0;
+}
+
+void matTranspose(float** M, float** T) {
+    for(size_t i = 0; i < N_SIZE; i++) {
+        for (size_t j = 0; j < N_SIZE; j++) {
+            T[j][i] = M[i][j];
+        }
+    }
+}
+
+bool checkSym(float** M) {
+    bool symmetric = true;
+    for (size_t i = 0; i < N_SIZE; i++) {
+        for (size_t j = 0; j < N_SIZE; j++) {
+            if(M[i][j] != M[j][i]) {
+                symmetric = false;
+            }
+        }
+    }
+    return symmetric;
+}
